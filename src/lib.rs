52bad3595850cda94bada8db7c28fd91539552b9
Diehard073055 <eshanshafeeq073055@gmail.com>
2023-02-26 13:07:56 +0800

    project setup

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use chrono::{Utc};
+use rust_decimal_macros::dec;
+mod exchange;
+use crate::exchange::transaction::Transaction;
+use crate::exchange::wallet::Wallet;
+
+fn main() {
+    println!("hello");
+}
+
+
