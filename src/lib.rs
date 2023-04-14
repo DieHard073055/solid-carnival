97fcaa8a0bdfd3c8221a175710c2684e36b5d0aa
Diehard073055 <eshanshafeeq073055@gmail.com>
2023-04-15 00:47:30 +0800

    Improve error handling for Exchanges struct methods
    - Add a `mut_unwrap_exchange_from_instance` method to handle instance retrieval and error management
    - Update `add_capital`, `add_price_feed`, and `tick` methods to return Result with appropriate error types

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,14 @@ pub mod exchange;
 
 use thiserror::Error;
 
-use std::collections::HashMap;
+use crate::exchange::exchange::{Exchange, ExchangeError};
+use crate::exchange::price_feed::PriceFeed;
 use chrono::Utc;
 use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
+use std::collections::HashMap;
+use thiserror::__private::AsDynError;
 use uuid::Uuid;
-use crate::exchange::exchange::{Exchange, ExchangeError};
-use crate::exchange::price_feed::PriceFeed;
 
 #[derive(Debug, Clone, Error)]
 pub enum ExchangesError {
@@ -17,7 +18,7 @@ pub enum ExchangesError {
 }
 
 struct Exchanges {
-    exchanges: HashMap<String, Exchange>
+    exchanges: HashMap<String, Exchange>,
 }
 impl Exchanges {
     fn create_new_exchange(&mut self) -> &str {
@@ -27,21 +28,44 @@ impl Exchanges {
         self.exchanges.insert(exchange_id.to_string(), exchange);
         exchange_id
     }
-    fn add_capital(&mut self, instance_id: &str, symbol: &str, amount: Decimal) -> Result<(), ExchangeError>{
+    fn mut_unwrap_exchange_from_instance(
+        &mut self,
+        instance_id: &str,
+    ) -> Result<&mut Exchange, ExchangesError> {
         if let Some(exchange) = self.exchanges.get_mut(instance_id) {
-            exchange.with_capital(vec![(symbol.to_string(), amount)])?
+            Ok(exchange)
+        } else {
+            Err(ExchangesError::InvalidExchangeId)
         }
+    }
+    fn add_capital(
+        &mut self,
+        instance_id: &str,
+        symbol: &str,
+        amount: Decimal,
+    ) -> Result<(), ExchangesError> {
+        let exchange = self.mut_unwrap_exchange_from_instance(instance_id)?;
+        exchange.with_capital(vec![(symbol.to_string(), amount)]);
         Ok(())
     }
-    fn add_price_feed(&mut self, instance_id: &str, symbol: &str, interval: &str, limit: i32){
-        if let Some(exchange) = self.exchanges.get_mut(instance_id) {
-            exchange.with_price_feed(symbol.to_string(), interval.to_string(), limit);
-        }
+    fn add_price_feed(
+        &mut self,
+        instance_id: &str,
+        symbol: &str,
+        interval: &str,
+        limit: i32,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+        let exchange = self.mut_unwrap_exchange_from_instance(instance_id)?;
+        exchange.with_price_feed(symbol.to_string(), interval.to_string(), limit)?;
+        Ok(())
     }
-    fn tick(&mut self, instance_id: &str){
-        if let Some(exchange) = self.exchanges.get_mut(instance_id) {
-            exchange.tick();
-        }
+    fn tick(
+        &mut self,
+        instance_id: &str,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+        let exchange = self.mut_unwrap_exchange_from_instance(instance_id)?;
+        exchange.tick()?;
+        Ok(())
     }
 }
 
@@ -49,44 +73,91 @@ impl Exchanges {
 mod test {
     use crate::exchange::order::{Order, OrderDirection, OrderType};
     use chrono::format::Numeric::Timestamp;
-    use rust_decimal_macros::dec;
-    use std::task::Context;
-    use rust_decimal::Decimal;
     use rust_decimal::prelude::ToPrimitive;
-    use uuid::Uuid;
+    use rust_decimal::Decimal;
+    use rust_decimal_macros::dec;
     use std::io::Write;
+    use std::task::Context;
+    use uuid::Uuid;
 
     fn fill_from_str(mut bytes: &mut [u8], s: &str) {
         bytes.write(s.as_bytes()).unwrap();
     }
-    fn create_order_uuid(pair: &str, price: Decimal, qty: Decimal, direction: OrderDirection, order_type: OrderType) {
-        let order = Order::new_order(
-            pair,
-            Some(price),
-            qty,
-            direction,
-            order_type
-        );
-        let order_direction = if order.direction == OrderDirection::Buy {0u8} else {1u8};
-        let order_type = if order.order_type == OrderType::Limit {0u8} else {1u8};
+    fn create_order_uuid(
+        pair: &str,
+        price: Decimal,
+        qty: Decimal,
+        direction: OrderDirection,
+        order_type: OrderType,
+    ) {
+        let order = Order::new_order(pair, Some(price), qty, direction, order_type);
+        let order_direction = if order.direction == OrderDirection::Buy {
+            0u8
+        } else {
+            1u8
+        };
+        let order_type = if order.order_type == OrderType::Limit {
+            0u8
+        } else {
+            1u8
+        };
         let order_price = order.price.unwrap_or(dec!(0));
-        let order_details = format!("{:}{:}{:}{:}{:}{:}", order.ts, order.pair, order_price, order.qty, order_direction, order_type);
+        let order_details = format!(
+            "{:}{:}{:}{:}{:}{:}",
+            order.ts, order.pair, order_price, order.qty, order_direction, order_type
+        );
         let mut order_details_bytes: [u8; 16] = [0; 16];
         fill_from_str(&mut order_details_bytes, order_details.as_str());
         println!("{:}", order_details);
         println!("{:?}", order_details.as_bytes());
         println!("{:?}", order_details_bytes);
         let order_id = Uuid::from_slice(&order_details_bytes).unwrap();
-        println!("order_id : {:}", order_id );
+        println!("order_id : {:}", order_id);
     }
     #[test]
     fn test_gen_uuid() {
-        create_order_uuid("BTCUSDT", dec!(19000), dec!(1), OrderDirection::Buy, OrderType::Limit);
-        create_order_uuid("BTCUSDT", dec!(19000), dec!(1), OrderDirection::Buy, OrderType::Limit);
-        create_order_uuid("BTCUSDT", dec!(19500), dec!(1), OrderDirection::Buy, OrderType::Limit);
-        create_order_uuid("BTCUSDT", dec!(19000), dec!(0.1), OrderDirection::Buy, OrderType::Limit);
-        create_order_uuid("BTCUSDT", dec!(19000), dec!(0.1), OrderDirection::Sell, OrderType::Limit);
-        create_order_uuid("BTCUSDT", dec!(19000), dec!(1), OrderDirection::Buy, OrderType::Market);
+        create_order_uuid(
+            "BTCUSDT",
+            dec!(19000),
+            dec!(1),
+            OrderDirection::Buy,
+            OrderType::Limit,
+        );
+        create_order_uuid(
+            "BTCUSDT",
+            dec!(19000),
+            dec!(1),
+            OrderDirection::Buy,
+            OrderType::Limit,
+        );
+        create_order_uuid(
+            "BTCUSDT",
+            dec!(19500),
+            dec!(1),
+            OrderDirection::Buy,
+            OrderType::Limit,
+        );
+        create_order_uuid(
+            "BTCUSDT",
+            dec!(19000),
+            dec!(0.1),
+            OrderDirection::Buy,
+            OrderType::Limit,
+        );
+        create_order_uuid(
+            "BTCUSDT",
+            dec!(19000),
+            dec!(0.1),
+            OrderDirection::Sell,
+            OrderType::Limit,
+        );
+        create_order_uuid(
+            "BTCUSDT",
+            dec!(19000),
+            dec!(1),
+            OrderDirection::Buy,
+            OrderType::Market,
+        );
 
         // println!("order_id : {:}", order_id );
     }
